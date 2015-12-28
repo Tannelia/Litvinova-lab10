84e0d63de3f581e308d7e9a8b659b8607638c55a
Татьяна Литвинова <Tannelia@bk.ru>
2015-12-29 01:01:06 +0300

    Initial commit

---
+++ src/Litvinova.java
@@ -0,0 +1,29 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/*")
+public class Litvinova extends HttpServlet {
+    public Litvinova() {
+        super();
+    }
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getPathInfo();
+        name = name.substring(1, name.length());
+        response.setContentType("text/html; charset=utf-8");
+        PrintWriter out = response.getWriter();
+        if(name.equals("")) {
+            out.println("Введите ваше имя в адресной строке");
+        } else {
+            out.println("Ваше введённое имя: " + name);
+        }
+
+    }
+}
+
+
+
